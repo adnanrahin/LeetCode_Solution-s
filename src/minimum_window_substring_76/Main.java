e3d4545ecfa40455a25de19a191d9d99d84d96ce
Arx1971 <adnan.rahin13@gmail.com>
2020-01-30 12:15:08 -0500

    Window Substring

--- src/minimum_window_substring_76/Main.java
+++ src/minimum_window_substring_76/Main.java
@@ -5,59 +5,35 @@ import java.util.Map;
 
 public class Main {
 
-	public static void main(String args[]) {
-		System.out.println(minWindow("ADOBECODEBANC", "ABC"));
-	}
+    public static void main(String args[]) {
+        System.out.println(minWindow("ADOBECODEBANC", "ABC"));
+    }
 
-	public static String minWindow(String s, String t) {
+    public static String minWindow(String s, String t) {
 
-		String minimumSubString = new String();
+        int freq[] = new int[128];
 
-		Map<Character, Integer> hashtable = new HashMap<Character, Integer>();
+        for (int i = 0; i < t.length(); i++)
+            freq[t.charAt(i) - 'A']++;
 
-		for (int i = 0; i < t.length(); i++) {
-			if (!hashtable.containsKey(t.charAt(i)))
-				hashtable.put(t.charAt(i), 1);
-			else
-				hashtable.put(t.charAt(i), hashtable.get(t.charAt(i)) + 1);
-		}
+        int right = 0, left = 0, size = Integer.MAX_VALUE, counter = t.length(), ptr = 0;
 
-		Map<Character, Integer> seen = new HashMap<Character, Integer>();
+        while (right < s.length()) {
 
-		int curlen = 0;
-		int left = 0;
-		int minlen = Integer.MAX_VALUE;
+            if (freq[s.charAt(right++) - 'A']-- > 0)
+                counter--;
 
-		for (int i = 0; i < s.length(); i++) {
+            while (counter == 0) {
+                if (right - left < size) {
+                    size = right - left;
+                    ptr = left;
+                }
+                if (freq[s.charAt(left++) - 'A']++ == 0)
+                    counter++;
+            }
 
-			if (hashtable.containsKey(s.charAt(i))) {
-				if (seen.containsKey(s.charAt(i))) {
-					if (hashtable.get(s.charAt(i)) > seen.get(s.charAt(i))) {
-						curlen++;
-					}
-					seen.put(s.charAt(i), seen.get(s.charAt(i)) + 1);
-				} else {
-					seen.put(s.charAt(i), 1);
-					curlen++;
-				}
-			}
-			if (curlen == t.length()) {
-				char c = s.charAt(left);
-				while (!seen.containsKey(c) || hashtable.get(c) < seen.get(c)) {
-					if (seen.containsKey(c) && hashtable.get(c) < seen.get(c)) {
-						seen.put(c, seen.get(c) - 1);
-					}
-					left++;
-					c = s.charAt(left);
-				}
-				if (i - left + 1 < minlen) {
-					minimumSubString = s.substring(left, i + 1);
-					minlen = i - left + 1;
-				}
-			}
+        }
 
-		}
-
-		return minimumSubString;
-	}
+        return size == Integer.MAX_VALUE ? "" : s.substring(ptr, ptr + size);
+    }
 }
