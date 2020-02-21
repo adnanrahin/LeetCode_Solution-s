c93f7a13f15ea5578fb794e0e8dc900000b9c86d
Arx1971 <adnan.rahin13@gmail.com>
2020-02-21 18:01:09 -0500

    Binary Search Tree

---
+++ src/insert_into_a_binary_search_tree_701/Main.java
@@ -0,0 +1,37 @@
+package insert_into_a_binary_search_tree_701;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+
+    }
+
+    public class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+
+        if (root == null) {
+            root = new TreeNode(val);
+            return root;
+        }
+
+        if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        } else
+            root.left = insertIntoBST(root.left, val);
+
+        return root;
+    }
+
+}
