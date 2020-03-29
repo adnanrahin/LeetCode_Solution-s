ebfc5d7103b1dd61645250a476eafd0f18393326
Arx1971 <adnan.rahin13@gmail.com>
2020-03-29 03:17:25 -0400

    Linked List

--- src/reorder_list_143/Main.java
+++ src/reorder_list_143/Main.java
@@ -8,12 +8,6 @@ public class Main {
 
     public static void main(String args[]) {
 
-        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
-
-        for (int i = 0; i < list.size(); i++) {
-            
-        }
-
     }
 
     public class ListNode {
