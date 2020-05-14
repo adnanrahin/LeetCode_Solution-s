9b30f8f399310abeea0408239783aad4bdda6a7a
Arx1971 <adnan.rahin13@gmail.com>
2020-05-14 04:15:53 -0400

    Test

---
+++ src/sequential_digits_1291/Main.java
@@ -0,0 +1,31 @@
+package sequential_digits_1291;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+    }
+
+    static List<Integer> solution;
+
+    public List<Integer> sequentialDigits(int low, int high) {
+
+        solution = new ArrayList<>();
+
+        int baseNumber = low;
+
+
+        return solution;
+
+    }
+
+
+    public static void BackTrack(int low, int high) {
+
+    }
+
+
+}
