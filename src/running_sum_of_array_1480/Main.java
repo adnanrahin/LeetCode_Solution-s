e803f33699845f44fe99c08aa55ed21eb66419df
Arx1971 <adnan.rahin13@gmail.com>
2020-06-14 03:56:43 -0400

    Running Sum

---
+++ src/running_sum_of_array_1480/Main.java
@@ -0,0 +1,17 @@
+package running_sum_of_array_1480;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+    }
+
+    public int[] runningSum(int[] nums) {
+
+        for (int i = 1; i < nums.length; i++)
+            nums[i] += nums[i - 1];
+
+        return nums;
+    }
+
+}
