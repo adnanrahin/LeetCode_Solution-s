da770e3ffccf9a8e4e52daf04d3c9fe820367776
Arx1971 <adnan.rahin13@gmail.com>
2020-07-28 09:40:28 -0400

    Jump Game 55

--- src/jump_game_55/Main.java
+++ src/jump_game_55/Main.java
@@ -2,44 +2,30 @@ package jump_game_55;
 
 public class Main {
 
-	public static void main(String args[]) {
+    public static void main(String args[]) {
 
-		System.out.println(canJump(new int[] { 1, 2, 1, 1, 4 }));
-		System.out.println(canJump(new int[] { 3, 2, 1, 0, 1, 4 }));
-		System.out.println(canJump(new int[] { 0 }));
-		System.out.println(canJump(new int[] { 2, 0 }));
-		System.out.println(canJump(new int[] { 1, 1, 1, 0 }));
-		System.out.println(canJump(new int[] { 1, 2 }));
-		System.out.println(canJump(new int[] { 2, 0, 0 }));
-		System.out.println(canJump(new int[] { 2, 5, 0, 0 }));
-		System.out.println(canJump(new int[] { 3, 0, 8, 2, 0, 0, 1 }));
-	}
+        System.out.println(canJump(new int[]{1, 2, 1, 1, 4}));
+        System.out.println(canJump(new int[]{3, 2, 1, 0, 1, 4}));
+        System.out.println(canJump(new int[]{0}));
+        System.out.println(canJump(new int[]{2, 0}));
+        System.out.println(canJump(new int[]{1, 1, 1, 0}));
+        System.out.println(canJump(new int[]{1, 2}));
+        System.out.println(canJump(new int[]{2, 0, 0}));
+        System.out.println(canJump(new int[]{2, 5, 0, 0}));
+        System.out.println(canJump(new int[]{3, 0, 8, 2, 0, 0, 1}));
+    }
 
-	public static boolean canJump(int[] nums) {
+    public static boolean canJump(int[] nums) {
 
-		if (nums.length == 0 || nums.length == 1)
-			return true;
+        int max = nums[0];
 
-		if (nums[0] == 0 && nums.length > 1)
-			return false;
+        for (int i = 0; i < nums.length; i++) {
+            if (max >= nums.length - 1) return true;
+            if (i > max) return false;
+            max = Math.max(nums[i] + i, max);
+        }
 
-		if (nums.length == 2 && nums[0] > 0)
-			return true;
-
-		int p = 0;
-
-		for (int i = 0; i < nums.length; i++) {
-
-			if (p >= nums.length - 1) {
-				return true;
-			} else if (i > p)
-				return false;
-
-			p = Math.max(p, nums[i] + i);
-
-		}
-
-		return false;
-	}
+        return false;
+    }
 
 }
