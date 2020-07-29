66a1b780f11b5d69f46c89776b04aa8a87a6eb7b
Arx1971 <adnan.rahin13@gmail.com>
2020-07-28 20:20:31 -0400

    Spiral Matrix Fixed

--- src/spiral_matrix_54/Main.java
+++ src/spiral_matrix_54/Main.java
@@ -5,83 +5,37 @@ import java.util.List;
 
 public class Main {
 
-	public static void main(String args[]) {
+    public static void main(String args[]) {
 
-		System.out.println(spiralOrder(new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }));
-		System.out.println(spiralOrder(new int[][] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } }));
-		System.out.println(spiralOrder(new int[][] { { 3 }, { 2 } }));
-		System.out.println(spiralOrder(new int[][] { { 1, 2, 3 } }));
-		System.out.println(spiralOrder(new int[][] { { 7 }, { 9 }, { 6 } }));
-		System.out.println(spiralOrder(new int[][] { { 1, 2 }, { 3, 4 } }));
-		System.out.println(
-				spiralOrder(new int[][] { { 2, 3, 4 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 }, { 14, 15, 16 } }));
-	}
+        System.out.println(spiralOrder(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}));
+        System.out.println(spiralOrder(new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}}));
+        System.out.println(spiralOrder(new int[][]{{3}, {2}}));
+        System.out.println(spiralOrder(new int[][]{{1, 2, 3}}));
+        System.out.println(spiralOrder(new int[][]{{7}, {9}, {6}}));
+        System.out.println(spiralOrder(new int[][]{{1, 2}, {3, 4}}));
+        System.out.println(
+                spiralOrder(new int[][]{{2, 3, 4}, {5, 6, 7}, {8, 9, 10}, {11, 12, 13}, {14, 15, 16}}));
+    }
 
-	public static List<Integer> spiralOrder(int[][] mat) {
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> solution = new ArrayList<>();
 
-		List<Integer> list = new ArrayList<>();
+        if (matrix.length == 0) return solution;
+        int row = matrix.length;
+        int col = matrix[0].length;
 
-		try {
-			int row = mat.length;
-			int col = mat[0].length;
-			if (row == 1) {
-				for (int i = 0; i < col; i++) {
-					list.add(mat[0][i]);
-				}
-				return list;
-			}
+        for (int i = 0; i < (Math.min(row, col) + 1) / 2; i++) {
+            for (int j = i; j <= col - i - 1; j++)
+                solution.add(matrix[i][j]);
+            for (int j = i + 1; j < row - i - 1; j++)
+                solution.add(matrix[j][col - i - 1]);
+            for (int j = col - i - 1; row - i - 1 != i && j >= i; j--)
+                solution.add(matrix[row - i - 1][j]);
+            for (int j = row - i - 2; col - i - 1 != i && j > i; j--)
+                solution.add(matrix[j][i]);
 
-			if (col == 1) {
-				for (int i = 0; i < row; i++) {
-					list.add(mat[i][0]);
-				}
-				return list;
-			}
-
-			int l = 0;
-
-			for (int i = 0; i < row; i++) {
-
-				int j, k, m, o;
-
-				for (j = i; j < col - i - 1; j++) {
-					if (l == row * col)
-						break;
-					list.add(mat[i][j]);
-					l++;
-				}
-
-				for (k = i; k < row - i - 1; k++) {
-					if (l == row * col)
-						break;
-					list.add(mat[k][j]);
-					l++;
-				}
-
-				for (m = col - i - 1; m > i; m--) {
-					if (l == row * col)
-						break;
-					list.add(mat[k][m]);
-					l++;
-				}
-
-				for (o = row - i - 1; o > i; o--) {
-					if (l == row * col)
-						break;
-					list.add(mat[o][m]);
-					l++;
-				}
-
-			}
-
-			if (row == col && row * col % 2 != 0) {
-				list.add(mat[row / 2][col / 2]);
-			}
-
-		} catch (Exception e) {
-			return list;
-		}
-		return list;
-	}
+        }
+        return solution;
+    }
 
 }
