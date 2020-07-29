8dc9094b9b2e78b43c89c928efbbbe28bbaa2a19
Arx1971 <adnan.rahin13@gmail.com>
2020-07-29 19:55:58 -0400

    Binary

--- src/add_binary_67/Main.java
+++ src/add_binary_67/Main.java
@@ -7,7 +7,8 @@ public class Main {
         System.out.println(addBinary("11", "1"));
 
         System.out.println(addBinary("100111010101010110001010101010010100010001011",
-                "101010101010101010101010111110110110110110101101101101011011011010110110110101101101100000000000000001111111111111111111111111000101011010110110110101101101101101101"));
+                "10101010101010101010101011111011011011011010110110110101101101101011011011010110110110000000000000000" +
+                        "1111111111111111111111111000101011010110110110101101101101101101"));
 
     }
 
@@ -16,50 +17,20 @@ public class Main {
         int i = a.length() - 1;
         int j = b.length() - 1;
 
-        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 
-        char carry = '0';
+        int sum = 0;
 
-        while (i >= 0 && j >= 0) {
-            if (carry == '1' && a.charAt(i) == '1' && b.charAt(j) == '1') {
-                stringBuilder.append("1");
-            } else if ((a.charAt(i) == '1' && b.charAt(j) == '1') ||
-                    (a.charAt(i) == '1' && carry == '1') || (carry == '1' && b.charAt(j) == '1')) {
-                carry = '1';
-                stringBuilder.append("0");
-            } else {
-                char in = a.charAt(i) == '1' || b.charAt(j) == '1' || carry == '1' ? '1' : '0';
-                stringBuilder.append(in);
-                carry = '0';
-            }
-            i--;
-            j--;
-        }
-        while (i >= 0) {
-            if (carry == '1' && a.charAt(i) == '1') {
-                stringBuilder.append("0");
-            } else {
-                char in = a.charAt(i) == '1' || carry == '1' ? '1' : '0';
-                stringBuilder.append(in);
-                carry = '0';
-            }
-            i--;
+        while (i >= 0 || j >= 0) {
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            sb.append(sum % 2);
+            sum /= 2;
         }
 
-        while (j >= 0) {
-            if (carry == '1' && b.charAt(j) == '1') {
-                stringBuilder.append("0");
-            } else {
-                char in = b.charAt(j) == '1' || carry == '1' ? '1' : '0';
-                stringBuilder.append(in);
-                carry = '0';
-            }
-            j--;
-        }
+        if (sum >= 1) sb.append(sum);
 
-        if (carry == '1') stringBuilder.append(carry);
-
-        return stringBuilder.reverse().toString();
+        return sb.reverse().toString();
 
     }
 
