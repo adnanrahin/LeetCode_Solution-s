dbfeccd4ceea699cc357bf187785395117416c1e
Arx1971 <adnan.rahin13@gmail.com>
2020-07-28 21:29:44 -0400

    SpiralMatrix upate

--- src/spiral_matrix_II_59/Main.java
+++ src/spiral_matrix_II_59/Main.java
@@ -1,54 +1,40 @@
 package spiral_matrix_II_59;
 
+import java.util.Arrays;
+
 public class Main {
 
-	public static void main(String args[]) {
+    public static void main(String args[]) {
 
-		generateMatrix(1);
-		generateMatrix(2);
-		generateMatrix(3);
-		generateMatrix(4);
-		generateMatrix(5);
-		generateMatrix(9);
-		generateMatrix(6);
-		generateMatrix(7);
-		generateMatrix(8);
-		generateMatrix(9);
-		generateMatrix(10);
+        System.out.println(Arrays.deepToString(generateMatrix(1)));
+        System.out.println(Arrays.deepToString(generateMatrix(100)));
+        System.out.println(Arrays.deepToString(generateMatrix(1000)));
+        System.out.println(Arrays.deepToString(generateMatrix(10000)));
+        System.out.println(Arrays.deepToString(generateMatrix(100000)));
 
-	}
+    }
 
-	public static int[][] generateMatrix(int n) {
+    public static int[][] generateMatrix(int n) {
 
-		int l = 1;
-		int mat[][] = new int[n][n];
+        int[][] matrix = new int[n][n];
 
-		for (int i = 0; i < n/2; i++) {
+        int number = 1;
 
-			int j, k, m, o;
+        for (int i = 0; i < (n + 1) / 2; i++) {
+            for (int j = i; j < n - i; j++)
+                matrix[i][j] = number++;
 
-			for (j = i; j < n - i - 1; j++) {
-				mat[i][j] = l++;
-			}
+            for (int j = i + 1; j < n - i; j++)
+                matrix[j][n - i - 1] = number++;
 
-			for (k = i; k < n - i - 1; k++) {
-				mat[k][j] = l++;
-			}
+            for (int j = n - i - 2; j >= i; j--)
+                matrix[n - i - 1][j] = number++;
 
-			for (m = n - i - 1; m > i; m--) {
-				mat[k][m] = l++;
-			}
+            for (int j = n - i - 2; j > i; j--)
+                matrix[j][i] = number++;
+        }
 
-			for (o = n - i - 1; o > i; o--) {
-				mat[o][m] = l++;
-			}
-		}
-
-		if (n % 2 != 0)
-			mat[n / 2][n / 2] = n * n;
-
-		return mat;
-
-	}
+        return matrix;
+    }
 
 }
