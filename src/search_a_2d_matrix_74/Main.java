f3479ca7adab20ab298b2b2e0f6faf6cb2a7778d
Arx1971 <adnan.rahin13@gmail.com>
2020-07-29 22:03:56 -0400

    Search In Sorted Matrix

--- src/search_a_2d_matrix_74/Main.java
+++ src/search_a_2d_matrix_74/Main.java
@@ -2,38 +2,28 @@ package search_a_2d_matrix_74;
 
 public class Main {
 
-	public static void main(String args[]) {
+    public static void main(String args[]) {
 
-		int matrix_1[][] = { { 1, 3, 5, 7, }, { 10, 11, 16, 20 }, { 23, 30, 34, 50 } };
-		System.out.println(searchMatrix(matrix_1, 11));
+        int[][] matrix = {{1, 3, 5, 7,}, {10, 11, 16, 20}, {23, 30, 34, 50}};
+        System.out.println(searchMatrix(matrix, 11));
 
-	}
+    }
 
-	public static boolean searchMatrix(int[][] matrix, int target) {
+    public static boolean searchMatrix(int[][] matrix, int target) {
 
-		try {
-			int counter = -1;
+        for (int[] ints : matrix) {
+            int lo = 0;
+            int high = matrix[0].length - 1;
 
-			for (int i = 0; i < matrix.length; i++) {
+            while (lo <= high) {
+                int mid = lo + (high - lo) / 2;
+                if (ints[mid] == target) return true;
+                else if (ints[mid] > target) high = mid - 1;
+                else lo = mid + 1;
+            }
+        }
+        return false;
 
-				if (target >= matrix[i][0] && target <= matrix[i][matrix[0].length - 1]) {
-					counter = i;
-					break;
-				}
-			}
-
-			for (int i = 0; i < matrix[counter].length; i++) {
-				if (matrix[counter][i] == target) {
-					return true;
-				}
-			}
-
-			return false;
-			
-		} catch (Exception e) {
-			// TODO: handle exception
-			return false;
-		}
-	}
+    }
 
 }
--- src/search_a_2d_matrix_II_240/Main.java
+++ src/search_a_2d_matrix_II_240/Main.java
@@ -2,45 +2,23 @@ package search_a_2d_matrix_II_240;
 
 public class Main {
 
-	public static void main(String args[]) {
-		int matrix_1[][] = { { 1, 3, 5, 7, }, { 10, 11, 16, 20 }, { 23, 30, 34, 50 } };
-		System.out.println(searchMatrix(matrix_1, 12));
-		int matrix_2[][] = { { 1, 4, 7, 11, 15 }, { 2, 5, 8, 12, 19 }, { 3, 6, 9, 16, 22 }, { 10, 13, 14, 17, 24 },
-				{ 18, 21, 23, 26, 30 } };
-		System.out.println(searchMatrix(matrix_2, 5));
-	}
+    public static void main(String args[]) {
+        int[][] matrix_1 = {{1, 3, 5, 7,}, {10, 11, 16, 20}, {23, 30, 34, 50}};
+        System.out.println(searchMatrix(matrix_1, 12));
+        int[][] matrix_2 = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24},
+                {18, 21, 23, 26, 30}};
+        System.out.println(searchMatrix(matrix_2, 5));
+    }
 
-	public static boolean searchMatrix(int[][] matrix, int target) {
-		try {
-			for(int i=0; i<matrix.length; i++) {
-				if(binray_Search(matrix[i], target)) {
-					return true;
-				}
-			}
-			return false;
-			
-		} catch (Exception e) {
-			// TODO: handle exception
-			return false;
-		}
-	}
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int row = matrix.length - 1;
+        int col = 0;
 
-	private static boolean binray_Search(int arr[], int target) {
-
-		int left = 0;
-		int right = arr.length - 1;
-
-		int mid;
-
-		while (left <= right) {
-			mid = (left + right) / 2;
-			if (arr[mid] == target)
-				return true;
-			else if (arr[mid] < target) {
-				left = mid + 1;
-			} else
-				right = mid - 1;
-		}
-		return false;
-	}
+        while (row >= 0 && col < matrix[0].length) {
+            if (matrix[row][col] == target) return true;
+            else if (matrix[row][col] > target) row--;
+            else col++;
+        }
+        return false;
+    }
 }
