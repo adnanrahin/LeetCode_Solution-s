7d1e6232a6f3138c779a0a5125235d6f995c09d5
Arx1971 <adnan.rahin13@gmail.com>
2020-08-03 06:29:38 -0400

    Code Update

--- src/pascals_triangle_118/Main.java
+++ src/pascals_triangle_118/Main.java
@@ -1,49 +1,36 @@
 package pascals_triangle_118;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 
-	public static void main(String args[]) {
+    public static void main(String args[]) {
 
-		System.out.println(generate(5));
-		System.out.println(generate(6));
+        System.out.println(generate(5));
+        System.out.println(generate(6));
 
-	}
+    }
 
-	public static List<List<Integer>> generate(int numRows) {
+    public static List<List<Integer>> generate(int numRows) {
 
-		List<List<Integer>> triangle = new ArrayList<>();
+        List<List<Integer>> triangle = new ArrayList<>();
 
-		if (numRows == 0)
-			return triangle;
-		if (numRows == 1) {
-			triangle.add(Arrays.asList(1));
-			return triangle;
-		}
+        for (int i = 1; i <= numRows; i++) {
+            List<Integer> local = new ArrayList<>();
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i) local.add(1);
+                else {
+                    int a = triangle.get(i - 2).get(j - 1);
+                    int b = triangle.get(i - 2).get(j - 2);
+                    local.add(a + b);
+                }
+            }
+            triangle.add(new ArrayList<>(local));
+        }
 
-		triangle.add(Arrays.asList(1));
-		triangle.add(Arrays.asList(1, 1));
-		for (int i = 2; i < numRows; i++) {
+        return triangle;
 
-			List<Integer> local = new ArrayList<>();
-
-			for (int j = 0; j <= i; j++) {
-				if (j == 0 || j == i) {
-					local.add(1);
-				} else {
-					local.add(triangle.get(i - 1).get(j - 1) + triangle.get(i - 1).get(j));
-				}
-			}
-
-			triangle.add(local);
-
-		}
-
-		return triangle;
-
-	}
+    }
 
 }
