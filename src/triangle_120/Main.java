f6cc6ddffa8d87545b628c750d03b665b433af29
Arx1971 <adnan.rahin13@gmail.com>
2020-08-03 07:53:40 -0400

    Code Upate

--- src/triangle_120/Main.java
+++ src/triangle_120/Main.java
@@ -4,51 +4,30 @@ import java.util.*;
 
 public class Main {
 
-	public static void main(String args[]) {
+    public static void main(String args[]) {
 
-		List<List<Integer>> test = new ArrayList<>();
+        List<List<Integer>> test = new ArrayList<>();
 
-		test.clear();
+        test.add(Arrays.asList(1));
+        test.add(Arrays.asList(-2, -5));
+        test.add(Arrays.asList(3, 6, 9));
+        test.add(Arrays.asList(-1, 2, 4, -3));
 
-		test.add(Arrays.asList(1));
-		test.add(Arrays.asList(-2, -5));
-		test.add(Arrays.asList(3, 6, 9));
-		test.add(Arrays.asList(-1, 2, 4, -3));
+        System.out.println(minimumTotal(test));
 
-		System.out.println(minimumTotal(test));
+    }
 
-	}
+    public static int minimumTotal(List<List<Integer>> triangle) {
 
-	public static int minimumTotal(List<List<Integer>> triangle) {
+        int[] dp = new int[triangle.size() + 1];
 
-		int m = triangle.size();
+        for (int i = triangle.size() - 1; i >= 0; i--) {
+            List<Integer> list = triangle.get(i);
+            for (int j = 0; j < list.size(); j++) {
+                dp[j] = Math.min(dp[j], dp[j + 1]) + list.get(j);
+            }
+        }
+        return dp[0];
 
-		int dp[][] = new int[m][m];
-
-		dp[0][0] = triangle.get(0).get(0);
-
-		for (int i = 1; i < triangle.size(); i++) {
-			
-			for (int j = 0; j < triangle.get(i).size(); j++) {
-				
-				if (j == 0)
-					dp[i][j] = dp[i - 1][j] + triangle.get(i).get(j);
-
-				else if (j == triangle.get(i).size() - 1)
-					dp[i][j] = dp[i - 1][j - 1] + triangle.get(i).get(j);
-
-				else
-					dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j]) + triangle.get(i).get(j);
-
-			}
-		}
-
-		int min = dp[m - 1][0];
-
-		for (int i = 0; i < dp[m - 1].length; i++)
-			if (min > dp[m - 1][i])
-				min = dp[m - 1][i];
-
-		return min;
-	}
+    }
 }
