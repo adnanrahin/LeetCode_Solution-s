d81d7158ce9986af9deadd6906a87605c592d544
Arx1971 <adnan.rahin13@gmail.com>
2020-08-13 15:28:26 -0400

    Kth Largest Element

--- src/kth_largest_element_in_an_array_215/Main.java
+++ src/kth_largest_element_in_an_array_215/Main.java
@@ -4,30 +4,28 @@ import java.util.PriorityQueue;
 
 public class Main {
 
-	public static void main(String args[]) {
+    public static void main(String args[]) {
 
-		System.out.println(findKthLargest(new int[] { 3, 2, 1, 5, 6, 4 }, 2));
-		System.out.println(findKthLargest(new int[] { 3, 2, 3, 1, 2, 4, 5, 5, 6 }, 4));
-		System.out.println(findKthLargest(new int[] { -1, 0, 1, 1, 2, 3 }, 4));
-	}
+        System.out.println(findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 2));
+        System.out.println(findKthLargest(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
+        System.out.println(findKthLargest(new int[]{-1, 0, 1, 1, 2, 3}, 4));
+    }
 
-	public static int findKthLargest(int[] nums, int k) {
-		int temp = nums[0];
+    public static int findKthLargest(int[] nums, int k) {
 
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-		for (int i = 0; i < nums.length; i++) {
-			pq.add(nums[i]);
-		}
-		int n = pq.size() - k;
-		int i = 0;
-		while (i < n) {
-			pq.remove();
-			i++;
-		}
-		temp = pq.peek();
-
-		return temp;
-	}
+        for (int i = 0; i < nums.length; i++) {
+            if (pq.size() < k) {
+                pq.add(nums[i]);
+            } else if (pq.size() == k) {
+                if (nums[i] > pq.peek()) {
+                    pq.poll();
+                    pq.add(nums[i]);
+                }
+            }
+        }
+        return pq.peek();
+    }
 
 }
