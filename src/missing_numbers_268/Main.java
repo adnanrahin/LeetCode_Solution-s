3958aca9512a2994012a10224f5452ec1c49fdd8
Arx1971 <adnan.rahin13@gmail.com>
2020-08-15 14:34:20 -0400

    Update

--- src/missing_numbers_268/Main.java
+++ src/missing_numbers_268/Main.java
@@ -4,40 +4,53 @@ import java.util.Arrays;
 
 public class Main {
 
-	public static void main(String args[]) {
-		System.out.println(missingNumber(new int[] { 3, 0, 1 }));
-		System.out.println(missingNumber(new int[] { 0 }));
-	}
+    public static void main(String args[]) {
+        System.out.println(missingNumber(new int[]{3, 0, 1}));
+        System.out.println(missingNumber(new int[]{0}));
+    }
 
-	public static int missingNumber(int[] nums) {
-		if (nums.length == 1 && nums[0] == 0)
-			return 1;
-		int n = nums.length;
-		int sum_of_n = ((n * n) + n) / 2;
-		int sum = 0;
-		for (int i = 0; i < nums.length; i++) {
-			sum += nums[i];
-		}
-		for (int i = 0; i < nums.length; i++) {
-			if (sum_of_n - i == sum) {
-				return i;
-			}
-		}
+    public static int missingNumber(int[] nums) {
+        if (nums.length == 1 && nums[0] == 0)
+            return 1;
+        int n = nums.length;
+        int sum_of_n = ((n * n) + n) / 2;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (sum_of_n - i == sum) {
+                return i;
+            }
+        }
 
-		return nums.length;
-	}
+        return nums.length;
+    }
 
-	public static int missingNumber_sort(int[] nums) {
-		if (nums.length == 1 && nums[0] == 0)
-			return 1;
-		Arrays.sort(nums);
-		for (int i = 0; i < nums.length; i++) {
+    static class Solution {
+        public static int missingNumber(int[] nums) {
 
-			if (nums[i] != i)
-				return i;
+            int sum1 = nums.length * (nums.length + 1) / 2;
+            int sum2 = 0;
 
-		}
-		return nums.length + 1;
-	}
+            for (int i = 0; i < nums.length; i++) {
+                sum2 += nums[i];
+            }
+            return sum1 - sum2;
+        }
+    }
+
+    public static int missingNumber_sort(int[] nums) {
+        if (nums.length == 1 && nums[0] == 0)
+            return 1;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] != i)
+                return i;
+
+        }
+        return nums.length + 1;
+    }
 
 }
