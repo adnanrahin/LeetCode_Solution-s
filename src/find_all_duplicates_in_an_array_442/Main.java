7dd42a0c766cbd0cb5d87408fd96ff67fc67ecd1
Arx1971 <adnan.rahin13@gmail.com>
2020-08-24 06:12:16 -0400

    Find Duplicates in an Array optimized Solution Added

--- src/find_all_duplicates_in_an_array_442/Main.java
+++ src/find_all_duplicates_in_an_array_442/Main.java
@@ -1,32 +1,26 @@
 package find_all_duplicates_in_an_array_442;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Main {
 
-	public static void main(String args[]) {
-		System.out.println(findDuplicates(new int[] {}));
-	}
+    public static void main(String args[]) {
+        System.out.println(findDuplicates(new int[]{}));
+    }
 
-	public static List<Integer> findDuplicates(int[] nums) {
+    public static List<Integer> findDuplicates(int[] nums) {
 
-		Set<Integer> set = new HashSet<Integer>();
+        List<Integer> solution = new ArrayList<>();
 
-		List<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] < 0) solution.add(Math.abs(index + 1));
+            else nums[index] = -nums[index];
+        }
 
-		for (int i = 0; i < nums.length; i++) {
+        return solution;
 
-			if (set.contains(nums[i])) {
-				list.add(nums[i]);
-			} else
-				set.add(nums[i]);
-
-		}
-
-		return list;
-	}
+    }
 
 }
