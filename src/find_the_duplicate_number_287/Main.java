5386a9479b97aa52b6618ab839a533739bc4f88b
Arx1971 <adnan.rahin13@gmail.com>
2020-08-27 16:32:16 -0400

    Java two poniter solution Added

--- src/find_the_duplicate_number_287/Main.java
+++ src/find_the_duplicate_number_287/Main.java
@@ -1,23 +1,40 @@
 package find_the_duplicate_number_287;
 
 public class Main {
-	
-	public static void main(String args[]) {
-		System.out.println(findDuplicate(new int [] {-3,1,-3,4,2}));
-	}
-	
-	public static int findDuplicate(int []arr) {
-		
-		for(int i = 0 ; i < arr.length; i++) {
-			
-			if(arr[Math.abs(arr[i])] >=0) {
-				arr[Math.abs(arr[i])] = - arr[Math.abs(arr[i])];
-			}
-			else return Math.abs(arr[i]);
-			
-		}
-		
-		return 0;
-	}
-	
+
+    public static void main(String[] args) {
+        System.out.println(findDuplicate(new int[]{-3, 1, -3, 4, 2}));
+    }
+
+    public static int findDuplicate(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[Math.abs(arr[i])] >= 0) {
+                arr[Math.abs(arr[i])] = -arr[Math.abs(arr[i])];
+            } else return Math.abs(arr[i]);
+
+        }
+
+        return 0;
+    }
+
+    public static class TwoPointerSolution {
+        public int findDuplicate(int[] nums) {
+            int fast = nums[nums[0]];
+            int slow = nums[0];
+
+            while (fast != slow) {
+                fast = nums[nums[fast]];
+                slow = nums[slow];
+            }
+            fast = 0;
+            while (fast != slow) {
+                fast = nums[fast];
+                slow = nums[slow];
+            }
+            return fast;
+        }
+    }
+
 }
