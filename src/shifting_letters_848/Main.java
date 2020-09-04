d8f37fc45841d0dad29af896a61018e40d344ab9
Arx1971 <adnan.rahin13@gmail.com>
2020-09-04 15:29:48 -0400

    Optmized Solution Added

--- src/shifting_letters_848/Main.java
+++ src/shifting_letters_848/Main.java
@@ -2,43 +2,48 @@ package shifting_letters_848;
 
 public class Main {
 
-	public static void main(String args[]) {
-		System.out.println(shiftingLetters("abc", new int[] { 3, 5, 9 }));
-		System.out.println(shiftingLetters("zzz", new int[] { 3, 3, 3 }));
-		System.out.println(shiftingLetters("bad", new int[] { 10, 20, 30 }));
-		System.out.println(shiftingLetters("ktmq", new int[] { 10, 26, 8, 2 }));
+	public static void main(String[] args) {
+
+		System.out.println(new PrefixSumSolution().shiftingLetters("mkgfzkkuxownxvfvxasy",
+				new int[]{505870226, 437526072, 266740649, 224336793, 532917782, 311122363, 567754492, 595798950,
+						81520022, 684110326, 137742843, 275267355, 856903962, 148291585, 919054234, 467541837,
+						622939912, 116899933, 983296461, 536563513}));
+
 	}
 
-	public static String shiftingLetters(String S, int[] shifts) {
+	public static class PrefixSumSolution {
+		public String shiftingLetters(String S, int[] shifts) {
 
-		StringBuffer sb = new StringBuffer();
+			char[] charArray = S.toCharArray();
 
-		long sum = 0;
+			long sum = 0;
 
-		for (int i = 0; i < shifts.length; i++) {
-			sum += shifts[i];
-		}
+			for (int i = 0; i < shifts.length; i++) sum += shifts[i];
 
-		for (int i = 0; i < S.length(); i++) {
-
-			long mod = sum % 26;
-
-			if (S.charAt(i) + mod > 'z') {
-
-				long m = S.charAt(i) + mod - 1;
-				long n = m - 'z';
-				char a = (char) (n + 'a');
-				sb.append(a);
-			} else {
-				char r = (char) (S.charAt(i) + mod);
-				sb.append(r);
+			for (int i = 0; i < shifts.length; i++) {
+				long shift = ((charArray[i] - 'a') + sum) % 26;
+				charArray[i] = (char) (shift + 'a');
+				sum -= shifts[i];
 			}
 
-			sum -= shifts[i];
-
+			return new String(charArray);
 		}
-		return sb.toString();
+	}
 
+	public static class BruteForce { // Brute Force solution will lead us to the TLE
+		public static String shiftingLetters(String S, int[] shifts) {
+
+			char[] charArray = S.toCharArray();
+
+			for (int i = 1; i <= shifts.length; i++) {
+				for (int j = 0; j < i; j++) {
+					int shift = (charArray[j] + shifts[i - 1]) % 'z';
+					charArray[j] = (char) (shift);
+				}
+			}
+
+			return new String(charArray);
+		}
 	}
 
 }
