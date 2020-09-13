1cea6aae0a19d44b026d13cc8977115942dbdaad
Arx1971 <adnan.rahin13@gmail.com>
2020-09-13 18:52:49 -0400

    BellMan Ford Solution Added

--- src/network_delay_time_743/Main.java
+++ src/network_delay_time_743/Main.java
@@ -1,87 +1,109 @@
 package network_delay_time_743;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String args[]) {
+    public static void main(String args[]) {
 
-		System.out.println(networkDelayTime(new int[][] { { 2, 1, 1 }, { 2, 3, 1 }, { 3, 4, 1 } }, 4, 2));
-		System.out.println(networkDelayTime(new int[][] { { 1, 2, 1 } }, 2, 2));
-		System.out.println(networkDelayTime(new int[][] { { 1, 2, 1 }, { 2, 1, 3 } }, 2, 2));
-	}
+        System.out.println(new PriorityQueueSolution().networkDelayTime(new int[][]{{2, 1, 1}, {2, 3, 1}, {3, 4, 1}}, 4, 2));
+        System.out.println(new PriorityQueueSolution().networkDelayTime(new int[][]{{1, 2, 1}}, 2, 2));
+        System.out.println(new PriorityQueueSolution().networkDelayTime(new int[][]{{1, 2, 1}, {2, 1, 3}}, 2, 2));
+    }
 
-	public static int dist[];
-	public static List<node> adj[];
+    public static class PriorityQueueSolution {
+        public static int networkDelayTime(int[][] times, int N, int K) {
 
-	@SuppressWarnings("unchecked")
-	public static int networkDelayTime(int[][] times, int N, int K) {
+            Map<Integer, List<Node>> graph = new HashMap<>();
 
-		adj = (List<node>[]) new List[N + 1];
-		dist = new int[N + 1];
-		Arrays.fill(dist, 500);
+            for (int i = 1; i <= N; i++)
+                graph.put(i, new ArrayList<>());
 
-		for (int i = 0; i <= N; i++)
-			adj[i] = new ArrayList<>();
+            for (int i = 0; i < times.length; i++) {
+                graph.get(times[i][0]).add(new Node(times[i][1], times[i][2]));
+            }
 
-		for (int[] t : times) {
-			int src = t[0], dst = t[1], w = t[2];
-			adj[src].add(new node(dst, w));
-		}
+            Set<Integer> visited = new HashSet<>();
+            int[] distance = new int[N + 1];
+            Arrays.fill(distance, Integer.MAX_VALUE);
 
-		if (!dijkstra(K, N))
-			return -1;
+            dijkstra(K, graph, visited, distance);
 
-		int ans = 0;
-		
-		for (int i = 1; i <= N; i++)
-			ans = Math.max(ans, dist[i]);
+            if (N != visited.size()) return -1;
 
-		return ans;
-	}
+            int max = distance[1];
+            for (int i = 1; i < distance.length; i++)
+                max = Math.max(max, distance[i]);
 
-	public static boolean dijkstra(int src, int N) {
+            return max;
+        }
 
-		Set<Integer> visited = new HashSet<>();
-		PriorityQueue<node> pq = new PriorityQueue<>((a, b) -> (b.w - a.w));
-		pq.add(new node(src, 0));
-		dist[src] = 0;
+        public static void dijkstra(int src, Map<Integer, List<Node>> graph, Set<Integer> visited, int[] distance) {
 
-		while (!pq.isEmpty()) {
+            PriorityQueue<Node> q = new PriorityQueue<>();
+            visited.add(src);
+            distance[src] = 0;
+            q.add(new Node(src, 0));
 
-			node n = pq.poll();
-			int u = n.u;
-			visited.add(u);
+            while (!q.isEmpty()) {
 
-			for (node i : adj[u]) {
+                Node parent = q.poll();
+                visited.add(parent.adj);
 
-				int v = i.u;
+                List<Node> adjacent = graph.get(parent.adj);
 
-				if (dist[u] + i.w < dist[v]) {
-					dist[v] = dist[u] + i.w;
-					pq.add(new node(v, dist[v]));
-				}
+                for (int i = 0; i < adjacent.size(); i++) {
+                    Node child = adjacent.get(i);
+                    int v = child.adj;
+                    int cst = child.cost;
+                    if (!visited.contains(v)) {
+                        if (distance[v] > distance[parent.adj] + cst) {
+                            distance[v] = distance[parent.adj] + cst;
+                            q.add(new Node(v, distance[v]));
+                        }
+                    }
+                }
+            }
+        }
 
-			}
+        public static class Node implements Comparable<Node> {
+            int adj;
+            int cost;
 
-		}
+            public Node(int adj, int cost) {
+                this.adj = adj;
+                this.cost = cost;
+            }
 
-		return visited.size() == N;
+            public int compareTo(Node node) {
+                return this.cost - node.cost;
+            }
 
-	}
+        }
+    }
 
-	public static class node {
-		int u, w;
+    public static class BellManFord {
+        public int networkDelayTime(int[][] times, int N, int K) {
+            int[] dist = new int[N + 1];
+            Arrays.fill(dist, 0x7fffffff);
+            dist[K] = 0;
+            for (int i = 0; i < N; i++) {
+                for (int[] edge : times) {
+                    int u = edge[0];
+                    int v = edge[1];
+                    int cost = edge[2];
+                    if (dist[u] != Integer.MAX_VALUE && dist[u] + cost < dist[v]) dist[v] = dist[u] + cost;
+                }
+            }
 
-		public node(int u, int w) {
-			this.u = u;
-			this.w = w;
-		}
-	}
+            int max = dist[1];
+
+            for (int i = 1; i < dist.length; i++) {
+                if (dist[i] == 0x7fffffff) return -1;
+                max = Math.max(dist[i], max);
+            }
+            return max;
+        }
+    }
 
 }
