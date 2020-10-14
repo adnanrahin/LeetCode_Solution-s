f39643dbdb9c9add74f38daeac3084a4708a66fa
Arx1971 <adnan.rahin13@gmail.com>
2020-10-14 19:33:36 -0400

    Find and Union Method are Added to the function

--- src/redundant_connection_II_685/Main.java
+++ src/redundant_connection_II_685/Main.java
@@ -3,7 +3,27 @@ package redundant_connection_II_685;
 public class Main {
 
     public static void main(String[] args) {
+
+    }
+
+    public int[] findRedundantDirectedConnection(int[][] edges) {
+
+        int[] redundantConnection = new int[2];
         
+
+        return redundantConnection;
+
+    }
+
+    public static int find(int[] dsu, int x) {
+        if (x == dsu[x]) return x;
+        return find(dsu, dsu[x]);
+    }
+
+    public static void union(int[] dsu, int x, int y) {
+        int xp = find(dsu, x);
+        int yp = find(dsu, y);
+        dsu[yp] = xp;
     }
 
 }
