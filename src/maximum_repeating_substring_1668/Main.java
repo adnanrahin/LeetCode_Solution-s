bac3622aacf1ec29d12acef487a7080acd7d356c
Adnan Rahin <adnan.rahin13@gmail.com>
2020-12-11 22:09:41 -0500

    Java Iterative Solution

--- src/maximum_repeating_substring_1668/Main.java
+++ src/maximum_repeating_substring_1668/Main.java
@@ -1,45 +1,28 @@
 package maximum_repeating_substring_1668;
 
-import java.util.Arrays;
-
 public class Main {
 
-    public static void main(String[] args) {
+    /*[0, 1, 2, 0, 1, 0, 1, 2, 3, 4, 5,
+    2, 3, 3, 4, 5, 2, 3, 4, 5,
+    2, 3, 3, 4, 5,
+    2, 3, 3, 4, 5,
+    2, 3, 3, 4, 5,
+    2, 3, 3, 4, 5]*/
 
-        System.out.println(Arrays.toString(piTable("aafdfdab a aaab aaab a aaab a aaab a aaab a aaaba", "aaaba")));
+    public static void main(String[] args) {
 
     }
 
     public static int maxRepeating(String sequence, String word) {
 
-        int[] pi = piTable(sequence, word);
-        int counter = 0;
-        for (int i = word.length(); i < pi.length; i++) {
-            if (pi[i] == word.length()) counter++;
-        }
-        return counter;
-    }
+        int count = 0;
+        String str = word;
 
-    public static int[] piTable(String text, String pattern) {
-        String str = pattern + "#" + text;
-        int[] pi = new int[str.length()];
-        int i = 0, j = 1;
-
-        while (j < str.length()) {
-            if (str.charAt(i) == str.charAt(j)) {
-                pi[j] = i + 1;
-                i++;
-                j++;
-            } else {
-                if (i != 0) {
-                    i = pi[i - 1];
-                } else {
-                    pi[j] = 0;
-                    j++;
-                }
-            }
+        while (sequence.contains(str)) {
+            count++;
+            str += word;
         }
-        return pi;
+        return count;
     }
 
 }
